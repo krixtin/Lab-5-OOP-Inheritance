d4486a5dbca80dcb6258a6b04e59e7b69e8ad003
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-10-01 16:24:15 +0200

    + llama class

--- Classes/Animals/Mammals/Llama.cs
+++ Classes/Animals/Mammals/Llama.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace Lab_5_OOP_Inheritance.Classes.Animals.Mammals
 {
-    internal class Llama //: Mammal
+    internal class Llama : Mammal
     {
+        public bool IsWearingHat { get; set; }
+
+        public Llama(string species, string name, int age, int numOfLegs, bool isTame, string habitat, int numOfNipples, bool isWearingHat) : base(species, name, age, numOfLegs, isTame, habitat, numOfNipples)
+        {
+            Name = name;
+            Age = age;
+            NumOfLegs = numOfLegs;
+            IsTame = isTame;
+            Habitat = habitat;
+            NumOfNipples = numOfNipples;
+            IsWearingHat = isWearingHat;
+        }
+
+        public override void Introduce()
+        {
+            base.Introduce();
+            if (IsWearingHat)
+            {
+                Console.WriteLine($"{Name} har på sig en hatt! Vad gulligt!");
+            }
+        }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} bräker!");
+        }
+
+        public override void Move()
+        {
+            Console.WriteLine($"{Name} skuttar omkring.");
+        }
+
+        public override void HaveKids(int numOfKids)
+        {
+            if (numOfKids == 1)
+            {
+                Console.WriteLine($"{Name} har fått en gullig liten lamaunge!");
+            }
+            else if (numOfKids == 2)
+            {
+                Console.WriteLine($"{Name} har fått tvillingar! Lamor kan få det, men det är ovanligt!");
+            }
+            else
+            {
+                Console.WriteLine("Lamor kan inte få så många ungar, tror jag.");
+            }
+        }
+
+        public void Spit()
+        {
+            Console.WriteLine($"{Name} spottar. Akta!");
+        }
+
+
     }
 }
