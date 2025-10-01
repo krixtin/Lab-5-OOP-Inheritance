782fe85d7ab0a00ee8c3069f087987fbef13373d
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-10-01 15:54:16 +0200

    testing some classes

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
-﻿namespace Lab_5_OOP_Inheritance
+﻿using Lab_5_OOP_Inheritance.Classes.Animals.Mammals;
+
+namespace Lab_5_OOP_Inheritance
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Cat kattenLaima = new Cat("katt", "Laima", 9, 4, true, "i en lägenhet", 8, true);
+            kattenLaima.Introduce();
+            kattenLaima.Eat();
+            kattenLaima.Purr();
+
+            Human jag = new Human("människa", "Kristin", 36, 2, false, "i en lägenhet", 2, 158);
+            jag.Introduce();
+            jag.Cook();
+            jag.HaveKids(1);
+
 
         }
     }
