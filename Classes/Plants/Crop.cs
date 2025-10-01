165d0bc54b0ea28ed3445890a25149f7e465f24a
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-10-01 13:35:18 +0200

    Add project files.

---
+++ Classes/Plants/Crop.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_5_OOP_Inheritance.Classes.Plants
+{
+    internal class Crop : Plant
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab_5_OOP_Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
