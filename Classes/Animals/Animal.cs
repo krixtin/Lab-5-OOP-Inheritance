d9e685aaa8d8a073c940d36b04e603fcee017ba1
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-10-01 14:23:35 +0200

    created animal class

--- Classes/Animals/Animal.cs
+++ Classes/Animals/Animal.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Lab_5_OOP_Inheritance.Classes.Animals
 {
-    internal class Animal
+    internal abstract class Animal
     {
+        string Name { get; set; }
+        int Age { get; set; }
+        int NumOfLegs { get; set; }
+        bool IsTame { get; set; }
+        string Habitat { get; set; }
+
+        public Animal(string name, int age, int numOfLegs, bool isTame, string habitat)
+        {
+            Name = name;
+            Age = age;
+            NumOfLegs = numOfLegs;
+            IsTame = isTame;
+            Habitat = habitat;
+        }
+
+        public abstract void MakeSound();
+
+        public abstract void Move();
+
+        public virtual void Eat()
+        {
+            Console.WriteLine($"{Name} äter.");
+        }
+
     }
 }
