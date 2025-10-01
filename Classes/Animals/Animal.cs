326c8b5aa2381a42c0e98d184a857c2f24638c91
Kristin Ahnlund <kristin.ahnlund@gmail.com>
2025-10-01 15:08:08 +0200

    + introduce method + species property

--- Classes/Animals/Animal.cs
+++ Classes/Animals/Animal.cs
@@ -8,14 +8,16 @@ namespace Lab_5_OOP_Inheritance.Classes.Animals
 {
     internal abstract class Animal
     {
+        public string Species { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
         public int NumOfLegs { get; set; }
         public bool IsTame { get; set; }
         public string Habitat { get; set; }
 
-        public Animal(string name, int age, int numOfLegs, bool isTame, string habitat)
+        public Animal(string species, string name, int age, int numOfLegs, bool isTame, string habitat)
         {
+            Species = species;
             Name = name;
             Age = age;
             NumOfLegs = numOfLegs;
@@ -23,7 +25,19 @@ namespace Lab_5_OOP_Inheritance.Classes.Animals
             Habitat = habitat;
         }
 
-        public abstract void Introduce();
+        public virtual void Introduce()
+        {
+            Console.WriteLine($"{Name} är en {Species} som är {Age} år gammal. {Name} har {NumOfLegs} ben och bor {Habitat}.");
+            if (IsTame)
+            {
+                Console.WriteLine($"{Name} är tam. Du får klappa om du vill!");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} är inte tam. Var försiktig!");
+            }
+        }
+
         public abstract void MakeSound();
 
         public abstract void Move();
